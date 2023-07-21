df0b87d3c7e464de18134ffddc83382cb35d376a
KrownWealth <abdulramonadeola36@gmail.com>
2023-07-21 16:54:26 +0100

    Rendered work-box dynamically using javascript

--- script.js
+++ script.js
@@ -73,21 +73,3 @@ document.body.addEventListener("keydown", (e) => {
     btnToggleNav.focus();
   }
 });
-
-const logosWrappers = document.querySelectorAll(".logo-group");
-
-const sleep = (number) => new Promise((res) => setTimeout(res, number));
-
-logosWrappers.forEach(async (logoWrapper, i) => {
-  const logos = Array.from(logoWrapper.children);
-  await sleep(1400 * i);
-  setInterval(() => {
-    let temp = logos[0];
-    logos[0] = logos[1];
-    logos[1] = logos[2];
-    logos[2] = temp;
-    logos[0].classList.add("hide", "to-top");
-    logos[1].classList.remove("hide", "to-top", "to-bottom");
-    logos[2].classList.add("hide", "to-bottom");
-  }, 5600);
-});
---
+++ workData.js
@@ -0,0 +1,164 @@
+let workData = [
+
+  {
+    id: 0,
+    workTitle: "deski VR website",
+    workText: "Deski delivered blazing performance, striking word solution. Built with:",
+    source: "https://deski-vr-website.vercel.app/",
+    img: "./assets/images/Deski-Saas-Software-HTML-Template.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "HTML",
+        skill2: "CSS3",
+        skill3: "JavaScript",
+      }
+
+    ]
+  },
+  {
+    id: 1,
+    workTitle: "clothccessories.com",
+    workText: "An e-ccommerce shopping website, with add-to-cart functionality and accept payment processing. Created with :",
+    source: "https://github.com/KrownWealth/Clothccessories",
+    img: "./assets/images/myclothccessories.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "HTML",
+        skill2: "CSS3",
+        skill3: "JavaScript",
+        skill4: "Bootstrap"
+      }
+
+    ]
+  },
+  {
+    id: 2,
+    workTitle: "Ascentfoods.com",
+    workText: "Converted Figma design to a pixel-perfect wordpress website. Designed with:",
+    source: "https://ascentfoods.com/",
+    img: "./assets/images/Home-Ascent-Foods.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "WordPress"
+      }
+
+    ]
+  },
+  {
+    id: 3,
+    workTitle: "Rennie's blog",
+    workText: "Redesign Rennie's blog to a professional wordpress blog design. Redesigned using:",
+    source: "https://renniecurran.com/news-grid/",
+    img: "./assets/images/Blog-Rennie-Curran.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "WordPress",
+        skill2:"Elementor Pro"
+      }
+
+    ]
+  },
+  {
+    id: 4,
+    workTitle: "PetMe",
+    workText: "Contributed to an open-source PetMe project. Improved the each-pet profile card design.",
+    source: "https://akshitagupta15june.github.io/PetMe/",
+    img: "./assets/images/PetMe.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "HTML",
+        skill2: "Tailwind CSS",
+        skill3: "JavaScript",
+        
+      }
+
+    ]
+  },
+  {
+    id: 5,
+    workTitle: "ThePlace.com.ng",
+    workText: "Developed an online restaurant food ordering system.",
+    source: "https://theplace.com.ng/",
+    img: "./assets/images/RestaurantThePlace.png",
+    btn: "Explore this project",
+    skills: [{
+        skill1: "WordPress"
+      }
+
+    ]
+  },
+]
+
+
+const itemsToShowInitially = 3;
+let currentItemsToShow = itemsToShowInitially;
+
+const workContainer = document.getElementById('workContainer');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+
+let generateWork = () => {
+  workContainer.innerHTML = workData
+  const contentToDisplay = workData
+    .slice(0, currentItemsToShow)
+    .map((x) => {
+      let {
+        id,
+        workTitle,
+        workText,
+        source,
+        img,
+        btn,
+        skills
+      } = x;
+      const skillsList = skills.map((skill) =>
+        `<li>${skill.skill1}</li>
+         <li>${skill.skill2}
+        </li><li>${skill.skill3}</li>
+        <li>${skill.skill4}</li>`
+      ).join('');
+
+
+      return `
+      <div class="work-box">
+      <div class="work-textbox">
+        <h3 class="h3">${workTitle}</h3>
+        <p class="work-text">${workText}</p>
+        <ol class="work-technologies">
+        ${skillsList}
+      </ol>
+      <div class="work-links">
+          <a href=${source} target="_blank" rel="noopener" class="link">${btn}</a>
+          <a href=${source} target="_blank" rel="noopener" title="Source code">
+          <img src=${img} alt="GitHub" loading="lazy" />
+          </a>
+        </div>
+      </div>
+      <picture class="work-img">
+        <img loading="lazy" src=${img} alt="Clothccessories"/>
+      </picture>
+    </div>
+      `;
+    })
+    .join("");
+
+  workContainer.innerHTML = contentToDisplay;
+
+
+  workContainer.innerHTML = contentToDisplay;
+  if (currentItemsToShow >= workData.length) {
+    loadMoreBtn.style.display = "none";
+  } else {
+    loadMoreBtn.style.display = "block";
+  }
+};
+
+
+loadMoreBtn.addEventListener("click", function () {
+  currentItemsToShow += 1;
+
+  generateWork();
+});
+
+
+generateWork();
